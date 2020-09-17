13c99c673bbd57fc816bf781d8f4cf633cea94e5
TeganHakim <67878686+TeganHakim@users.noreply.github.com>
2020-09-16 21:42:34 -0400

    First game commit

---
+++ Pick Your Poison/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float turnSpeed = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.up * verticalInput * movementSpeed * Time.deltaTime);
+        transform.Translate(Vector3.right * horizontalInput * turnSpeed * Time.deltaTime);
+    }
+}
